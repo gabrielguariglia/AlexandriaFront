3fce497e64b2ac7c57afd1e9ec86ab7615153b7d
gabrielguariglia <gabrielguariglia@gmail.com>
2022-09-29 23:28:00 -0300

    imagem placeolder colocada

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Container from 'react-bootstrap/Container'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import domcasmurro from './Components/images/domcasmurro.png';
 
 function App() {
   return (
@@ -14,15 +15,15 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">Alexandria</Navbar.Brand>
           <Nav className="me-auto">
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
@@ -30,30 +31,20 @@ function App() {
         </Container>
       </Navbar>
 
-      
-        <Row className="justify-content-center">
-          <Card style={{ margin:'15%', width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Placeholder</Card.Title>
-              <Card.Text>
-                Texto
-              </Card.Text>
-              <Button variant="primary">Ver Livro</Button>
-            </Card.Body>
-          </Card>
-          <Card style={{ margin:'15%', width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Placeholder</Card.Title>
-              <Card.Text>
-                Texto
-              </Card.Text>
-              <Button variant="primary">Ver Livro</Button>
-            </Card.Body>
-          </Card>
-        </Row>
-      
+
+      <Row className="justify-content-center">
+        <Card style={{ margin: '5%', left: '12cm', width: '15rem' }}>
+          <Card.Img variant="top" src={domcasmurro}/>
+          <Card.Body>
+            <Card.Title>Placeholder</Card.Title>
+            <Card.Text>
+              Texto
+            </Card.Text>
+            <Button variant="primary">Ver Livro</Button>
+          </Card.Body>
+        </Card>
+      </Row>
+
 
     </>
   );
