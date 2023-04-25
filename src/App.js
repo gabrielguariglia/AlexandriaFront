0fd514cb7b41ab0e7b788537776151ec1941f777
gabrielguariglia <gabrielguariglia@gmail.com>
2023-04-25 19:30:16 -0300

    Adicionado tela de livro

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import Form from 'react-bootstrap/Form'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import logo from './Components/images/logo.png';
-import saci from './Components/images/saci.png'
-import { Container, Row, Col } from 'react-bootstrap';
-import Table from 'react-bootstrap/Table';
-
+import { Nav, Button, Navbar, Form } from 'react-bootstrap';
+import logo from './Components/images/logo.png'
 
 function App() {
   return (
@@ -15,7 +8,7 @@ function App() {
       <Navbar bg="dark" variant="dark">
           <img style={{ width: 40, height: 50 }}
            src={logo} alt="React Logo"/>
-          <Navbar.Brand href="#home">Alexandria</Navbar.Brand>
+          <Navbar.Brand href="#Home">Alexandria</Navbar.Brand>
           <Nav className="me-auto">
             <Form className="d-flex">
               <Form.Control
@@ -31,30 +24,6 @@ function App() {
             <Nav.Link href="#Autores">Autores</Nav.Link>
           </Nav>
       </Navbar>
-
-      <Container className="justify-content-center">
-        <Col>
-          <Row>
-            <img style={{ margin: '6%', width: '25rem' }}
-              src={saci} alt="Imagem Livro" />
-            <Col>
-              <Table>
-                <tbody>
-                  <tr>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                  </tr>
-                  <tr>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                  </tr>
-                </tbody>
-              </Table>
-            </Col>
-          </Row>
-        </Col>
-      </Container>
-
     </>
   );
 }
---
+++ src/Components/Pages/Livro.js
@@ -0,0 +1,56 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import saci from './Components/images/saci.png'
+import { Container, Row, Col, Image, Table } from 'react-bootstrap';
+
+
+function Livro() {
+  return (
+    <>
+      <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Container style={{ height: '100%' }} className="d-flex align-items-center justify-content-center">
+          <Row>
+            <Col xs={12} md={4}>
+              <Image src={saci} fluid />
+            </Col>
+            <Col xs={12} md={8}>
+              <Table>
+                <thead>
+                  <tr>
+                    <th>Titulo</th>
+                    <th>O Saci</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Autor</td>
+                    <td>Monteiro Lobato</td>
+                  </tr>
+                  <tr>
+                    <td>Lançamento</td>
+                    <td>1921</td>
+                  </tr>
+                  <tr>
+                    <td>Língua</td>
+                    <td>Português</td>
+                  </tr>
+                  <tr>
+                    <td>Gênero</td>
+                    <td>Fantasia</td>
+                  </tr>
+                </tbody>
+              </Table>
+              <div className="d-flex justify-content-center mt-5">
+                <Button variant="danger" className="mx-2">Baixar PDF</Button>
+                <Button variant="primary" className="mx-2">Ler Online</Button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
+    </>
+  );
+}
+
+export default Livro;
