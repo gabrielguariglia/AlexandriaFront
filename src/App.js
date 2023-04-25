4651e39f3cea3954c15d6c7b6189f70adbef9832
gabrielguariglia <gabrielguariglia@gmail.com>
2023-04-25 20:35:47 -0300

    Adicionado routes (erro no navbar)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Nav, Button, Navbar, Form } from 'react-bootstrap';
-import logo from './Components/images/logo.png'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Navbar, Nav, Form, Button } from 'react-bootstrap';
+import logo from './Components/images/logo.png';
+
+function Home() {
+  return <h1>Home</h1>;
+}
 
 function App() {
   return (
     <>
       <Navbar bg="dark" variant="dark">
-          <img style={{ width: 40, height: 50 }}
-           src={logo} alt="React Logo"/>
-          <Navbar.Brand href="#Home">Alexandria</Navbar.Brand>
-          <Nav className="me-auto">
-            <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Pesquisar"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Pesquisar</Button>
-            </Form>
-            <Nav.Link href="#Home">Home</Nav.Link>
-            <Nav.Link href="#Livros">Livros</Nav.Link>
-            <Nav.Link href="#Autores">Autores</Nav.Link>
-          </Nav>
+        <Navbar.Brand href="#Home">
+          <img style={{ width: 40, height: 50, marginRight: 10 }} src={logo} alt="React Logo" />
+          Alexandria
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Pesquisar"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Pesquisar</Button>
+          </Form>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/livros">Livros</Nav.Link>
+          <Nav.Link href="/autores">Autores</Nav.Link>
+        </Nav>
       </Navbar>
+
+      <BrowserRouter>
+        <Routes>
+        <Route exact path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,40 +1,15 @@
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
 import Row from 'react-bootstrap/Row'
-import Form from 'react-bootstrap/Form'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import domcasmurro from './Components/images/domcasmurro.png';
 import saci from './Components/images/saci.png';
-import logo from './Components/images/logo.png';
 import mobydick from './Components/images/mobydick.png';
 
 function App() {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
-          <img style={{ width: 40, height: 50 }}
-           src={logo} alt="React Logo"/>
-          <Navbar.Brand href="#home">Alexandria</Navbar.Brand>
-          <Nav className="me-auto">
-            <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Pesquisar"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Pesquisar</Button>
-            </Form>
-            <Nav.Link href="#Home">Home</Nav.Link>
-            <Nav.Link href="#Livros">Livros</Nav.Link>
-            <Nav.Link href="#Autores">Autores</Nav.Link>
-          </Nav>
-      </Navbar>
-
-      <Row className="justify-content-center">
-
+    <Row className="justify-content-center">
         <Card style={{ margin: '5%', width: '15rem' }}>
           <Card.Img variant="top" src={domcasmurro}/>
           <Card.Body>
